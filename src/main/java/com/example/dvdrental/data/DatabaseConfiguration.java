afc2293b4c739c5c24dd67b683c0eaa99a66700a
Justin Robinson <justin.g.robinson@gmail.com>
2023-06-10 15:14:19 +0200

    wrap up

--- src/main/java/com/example/dvdrental/data/DatabaseConfiguration.java
+++ src/main/java/com/example/dvdrental/data/DatabaseConfiguration.java
@@ -1,12 +1,18 @@
 package com.example.dvdrental.data;
 
 import com.introproventures.graphql.jpa.query.schema.impl.GraphQLJpaSchemaBuilder;
+import graphql.schema.idl.SchemaPrinter;
 import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Configuration
 public class DatabaseConfiguration {
@@ -21,8 +27,14 @@ public class DatabaseConfiguration {
         try{
             GraphQLJpaSchemaBuilder schemaBuilder = new GraphQLJpaSchemaBuilder(entityManager);
             var build = schemaBuilder.build();
-            var def = build.getDefinition();
-            logger.info("GraphQL Schema: {}", def.toString());
+            String schemaString = new SchemaPrinter().print(build);
+            try {
+                Path output = Paths.get("schema.graphql");
+                logger.info("writing GraphQL Schema to file: {}", output.toAbsolutePath());
+                Files.write(output, schemaString.getBytes());
+            } catch (IOException e) {
+                logger.error("Error while writing schema to file", e);
+            }
         }catch (Exception e){
             logger.error("Error while generating GraphQL Schema", e);
             e.printStackTrace();
